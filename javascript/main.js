a5dd624038cbcd77ea8e2dd307f7fbd0386ac65f
alickchen <272988986@qq.com>
2016-05-29 20:21:10 +0800

    修改简历

--- javascript/main.js
+++ javascript/main.js
@@ -147,6 +147,23 @@ $scrollLi.click(function(){
 
 
 
+      $('.fancybox-thumbs').fancybox({
+        prevEffect : 'none',
+        nextEffect : 'none',
+
+        closeBtn  : false,
+        arrows    : false,
+        nextClick : true,
+
+        helpers : {
+          thumbs : {
+            width  : 50,
+            height : 50
+          }
+        }
+      });
+
+      
 
 
 
